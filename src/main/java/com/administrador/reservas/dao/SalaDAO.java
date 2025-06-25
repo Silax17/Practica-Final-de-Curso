e68cdb2b1f0b478ce43a9d75cbe9ebe4d21db7ee
ateres <aparejat17@gmail.com>
2025-06-25 21:19:31 +0200

    Terminado CRUD para SalaDAO
    Poner codigo que te dice el error que es

--- src/main/java/com/administrador/reservas/dao/SalaDAO.java
+++ src/main/java/com/administrador/reservas/dao/SalaDAO.java
@@ -13,56 +13,75 @@ import java.sql.ResultSet;
 import com.administrador.reservas.modelo.Sala;
 import com.administrador.reservas.ConexionBD;
 
-public class SalaDAO { //CRUD
-	//CREATE
-	public void agregarSala(Sala sala) throws SQLException{
-		String sql ="INSERT INTO SALA (nombre,capacidad,recursos_disponibles) VALUES(?,?,?)";
-		try(Connection con=ConexionBD.getConnection(); PreparedStatement st=con.prepareStatement(sql)){
-			st.setString(1,sala.getNombre());
-			st.setInt(2,sala.getCapacidad());
-			st.setString(3,sala.getRecursos_disponibles());
+public class SalaDAO { // CRUD
+	// CREATE
+	public void agregarSala(Sala sala) throws SQLException {
+		String sql = "INSERT INTO SALA (nombre,capacidad,recursos_disponibles) VALUES(?,?,?)";
+		try (Connection con = ConexionBD.getConnection(); PreparedStatement st = con.prepareStatement(sql)) {
+			st.setString(1, sala.getNombre());
+			st.setInt(2, sala.getCapacidad());
+			st.setString(3, sala.getRecursos_disponibles());
 			st.executeUpdate();
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			System.out.println("Error al insertar Sala");
-			
+			e.printStackTrace();
+
 		}
-		
-		
+
 	}
-	//READ
-	public List<Sala> verSalas() throws SQLException{
-		List<Sala> salaSQL=new ArrayList<>();
-		String sql="SELECT * FROM SALA";
-		try(Connection con=ConexionBD.getConnection(); Statement st=con.createStatement();ResultSet rs=st.executeQuery(sql)){
-			
-			while(rs.next()) {
-				Sala sala= new Sala();
+
+	// READ
+	public List<Sala> verSalas() throws SQLException {
+		List<Sala> salaSQL = new ArrayList<>();
+		String sql = "SELECT * FROM SALA";
+		try (Connection con = ConexionBD.getConnection();
+				Statement st = con.createStatement();
+				ResultSet rs = st.executeQuery(sql)) {
+
+			while (rs.next()) {
+				Sala sala = new Sala();
 				sala.setId(rs.getInt("id"));
 				sala.setNombre(rs.getString("nombre"));
 				sala.setCapacidad(rs.getInt("Capacidad"));
 				sala.setRecursos_disponibles(rs.getString("Recursos_disponibles"));
 				salaSQL.add(sala);
 			}
-			
-			
-		}catch(SQLException e) {
+
+		} catch (SQLException e) {
 			System.out.println("Error al ver salas");
-			
+			e.printStackTrace();
+
 		}
 		return salaSQL;
-		
+
 	}
-	//UPDATE
+
+	// UPDATE
 	public void actualizarSala(Sala sala) throws SQLException {
-		String sql="UPDATE SALA SET nombre=?, capacidad=?,recursos_disponibles=? WHERE ID=?";
-		try(Connection con=ConexionBD.getConnection(); PreparedStatement st=con.prepareStatement(sql)){
+		String sql = "UPDATE SALA SET nombre=?, capacidad=?,recursos_disponibles=? WHERE ID=?";
+		try (Connection con = ConexionBD.getConnection(); PreparedStatement st = con.prepareStatement(sql)) {
 			st.setString(1, sala.getNombre());
 			st.setInt(2, sala.getCapacidad());
 			st.setString(3, sala.getRecursos_disponibles());
-			st.setInt(4,sala.getId());
+			st.setInt(4, sala.getId());
 			st.executeUpdate();
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			System.out.println("Error al actualizar salas");
+			e.printStackTrace();
+		}
 	}
-}
+
+	// DELETE
+	public void eliminarSala(int idSala) throws SQLException {
+		String sql = "DELETE FROM SALA WHERE id=?";
+		try (Connection con = ConexionBD.getConnection(); PreparedStatement st = con.prepareStatement(sql)) {
+			st.setInt(1, idSala);
+			st.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("Error al eliminar sala");
+			e.printStackTrace();
+		}
+
+	}
 }

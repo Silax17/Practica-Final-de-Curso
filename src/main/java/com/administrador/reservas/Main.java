803316d30e0bdc46e8669280eaf1819492908f11
ateres <aparejat17@gmail.com>
2025-06-25 17:24:35 +0200

    Update Main.java
    Una prueba para saber si funciona la conexion con MySQL

--- src/main/java/com/administrador/reservas/Main.java
+++ src/main/java/com/administrador/reservas/Main.java
@@ -1,9 +1,20 @@
 package com.administrador.reservas;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
-	public static void main(String args[]) {
-		try(Connection con=ConeixonBD){
+	public static void main(String args[]) throws SQLException {
+		try(Connection con=ConexionBD.getConnection()){
+			if(con!=null && !con.isClosed()) {
+				System.out.println("Base de dades funcional");
+			}
+			else {
+				System.out.println("No funciona ");
+			}
 			
+		}catch(SQLException e) {
+			System.err.println("Error conectando a la base de datos:" + e.getMessage());
 		}
 			
 	}

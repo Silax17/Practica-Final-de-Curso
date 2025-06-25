1572eae84fce90a41828fa4692e1ae5df00dabcd
ateres <aparejat17@gmail.com>
2025-06-25 20:52:38 +0200

    Creada R (read)
    Hay tambien parte hecha de la U (update)

--- src/main/java/com/administrador/reservas/dao/SalaDAO.java
+++ src/main/java/com/administrador/reservas/dao/SalaDAO.java
@@ -6,7 +6,9 @@ import java.sql.Connection;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import com.administrador.reservas.modelo.Sala;
 import com.administrador.reservas.ConexionBD;
@@ -29,12 +31,36 @@ public class SalaDAO { //CRUD
 	}
 	//READ
 	public List<Sala> verSalas() throws SQLException{
-		List<Sala> salasSQL=new ArrayList<>();
+		List<Sala> salaSQL=new ArrayList<>();
 		String sql="SELECT * FROM SALA";
-		try(Connection con=ConexionBD.getConnection(); Statement st=con.createStatement()){
+		try(Connection con=ConexionBD.getConnection(); Statement st=con.createStatement();ResultSet rs=st.executeQuery(sql)){
 			
+			while(rs.next()) {
+				Sala sala= new Sala();
+				sala.setId(rs.getInt("id"));
+				sala.setNombre(rs.getString("nombre"));
+				sala.setCapacidad(rs.getInt("Capacidad"));
+				sala.setRecursos_disponibles(rs.getString("Recursos_disponibles"));
+				salaSQL.add(sala);
+			}
+			
+			
+		}catch(SQLException e) {
+			System.out.println("Error al ver salas");
 			
 		}
+		return salaSQL;
 		
+	}
+	//UPDATE
+	public void actualizarSala(Sala sala) throws SQLException {
+		String sql="UPDATE SALA SET nombre=?, capacidad=?,recursos_disponibles=? WHERE ID=?";
+		try(Connection con=ConexionBD.getConnection(); PreparedStatement st=con.prepareStatement(sql)){
+			st.setString(1, sala.getNombre());
+			st.setInt(2, sala.getCapacidad());
+			st.setString(3, sala.getRecursos_disponibles());
+			st.setInt(4,sala.getId());
+			st.executeUpdate()
+		}
 	}
 }

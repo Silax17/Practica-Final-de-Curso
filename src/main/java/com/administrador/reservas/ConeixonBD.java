674e7d60a4243a9c4cd3b3d7c2cd9c8b8566c9dc
ateres <aparejat17@gmail.com>
2025-06-25 16:19:50 +0200

    Conexion creada de Java-Mysql

--- src/main/java/com/administrador/reservas/ConeixonBD.java
+++ src/main/java/com/administrador/reservas/ConeixonBD.java
@@ -1,5 +1,17 @@
 package com.administrador.reservas;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConeixonBD {
+	private static final String URL ="jdbc:mysql://127.0.0.1:3307/DB_PROJ_FINAL";
+	private static final String username="root";
+	private static final String password="root";
+
+public static Connection getConnection() throws SQLException{
+	return DriverManager.getConnection(URL,username,password);
+	
+}
 
 }

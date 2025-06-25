4bb18a753d6211e715a3a5509ec678111e7747db
ateres <aparejat17@gmail.com>
2025-06-25 17:40:31 +0200

    Dependencies MySQL

--- src/main/java/com/administrador/reservas/ConexionBD.java
+++ src/main/java/com/administrador/reservas/ConexionBD.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-	private static final String URL ="jdbc:mysql://127.0.0.1:3307/DB_PROJ_FINAL";
+	private static final String URL ="jdbc:mysql://127.0.0.1:3307/pingu";
 	private static final String username="root";
 	private static final String password="root";
 

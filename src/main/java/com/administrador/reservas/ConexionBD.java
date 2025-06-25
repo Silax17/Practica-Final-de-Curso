096d126e1b01fd6d1b343d9da37f0e80917c8b09
ateres <aparejat17@gmail.com>
2025-06-25 20:28:10 +0200

    CRUD
    Creada la C (create) de SalaDAO

--- src/main/java/com/administrador/reservas/ConexionBD.java
+++ src/main/java/com/administrador/reservas/ConexionBD.java
@@ -5,13 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-	private static final String URL ="jdbc:mysql://127.0.0.1:3307/pingu";
-	private static final String username="root";
-	private static final String password="root";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3307/pingu";
+	private static final String username = "root";
+	private static final String password = "root";
 
-public static Connection getConnection() throws SQLException{
-	return DriverManager.getConnection(URL,username,password);
-	
-}
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(URL, username, password);
+
+	}
 
 }
--- src/main/java/com/administrador/reservas/dao/SalaDAO.java
+++ src/main/java/com/administrador/reservas/dao/SalaDAO.java
@@ -1,12 +1,40 @@
 package com.administrador.reservas.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Connection;
+
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 import com.administrador.reservas.modelo.Sala;
+import com.administrador.reservas.ConexionBD;
 
-public class SalaDAO {
+public class SalaDAO { //CRUD
+	//CREATE
 	public void agregarSala(Sala sala) throws SQLException{
-		String sql =""
+		String sql ="INSERT INTO SALA (nombre,capacidad,recursos_disponibles) VALUES(?,?,?)";
+		try(Connection con=ConexionBD.getConnection(); PreparedStatement st=con.prepareStatement(sql)){
+			st.setString(1,sala.getNombre());
+			st.setInt(2,sala.getCapacidad());
+			st.setString(3,sala.getRecursos_disponibles());
+			st.executeUpdate();
+		}catch(SQLException e) {
+			System.out.println("Error al insertar Sala");
+			
+		}
+		
+		
+	}
+	//READ
+	public List<Sala> verSalas() throws SQLException{
+		List<Sala> salasSQL=new ArrayList<>();
+		String sql="SELECT * FROM SALA";
+		try(Connection con=ConexionBD.getConnection(); Statement st=con.createStatement()){
+			
+			
+		}
 		
 	}
 }

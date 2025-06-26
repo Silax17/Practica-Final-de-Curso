57343e209d061785fb38515e030a720bf5c29cd6
ateres <aparejat17@gmail.com>
2025-06-26 16:40:44 +0200

    Creada D (Delete) Reserva DAO
    Se ha puesto el catch en //UPDATE

--- src/main/java/com/administrador/reservas/dao/ReservaDAO.java
+++ src/main/java/com/administrador/reservas/dao/ReservaDAO.java
@@ -28,13 +28,16 @@ public class ReservaDAO {
 			e.printStackTrace();
 		}
 	}
+
 //READ
 	public List<Reserva> verReserva() throws SQLException {
 		List<Reserva> reservaSQL = new ArrayList<>();
-		String sql="SELECT * FROM RESERVA";
-		try(Connection con = ConexionBD.getConnection(); Statement st=con.createStatement(); ResultSet rs= st.executeQuery(sql)){
-			while(rs.next()) {
-				Reserva reserva=new Reserva();
+		String sql = "SELECT * FROM RESERVA";
+		try (Connection con = ConexionBD.getConnection();
+				Statement st = con.createStatement();
+				ResultSet rs = st.executeQuery(sql)) {
+			while (rs.next()) {
+				Reserva reserva = new Reserva();
 				reserva.setId(rs.getInt("id"));
 				reserva.setEmpleado_id(rs.getInt("empleado_id"));
 				reserva.setSala_id(rs.getInt("sala_id"));
@@ -42,23 +45,39 @@ public class ReservaDAO {
 				reserva.setHora_inicio(rs.getTime("hora_inicio").toLocalTime());
 				reserva.setHora_final(rs.getTime("hora_final").toLocalTime());
 			}
-		}catch (SQLException e) {
+		} catch (SQLException e) {
 			System.out.println("Error al ver reserva");
 			e.printStackTrace();
 		}
 		return reservaSQL;
-		
+
 	}
+
 //UPDATE
-	public void actualizarReserva(Reserva reserva) throws SQLException{
-		String sql="UPDATE RESERVA SET empleado_id=?,sala_id=?,fecha=?,hora_inicio=?,hora_final=? WHERE id=?";
-		try (Connection con = ConexionBD.getConnection(); PreparedStatement st = con.prepareStatement(sql)){
-			st.setInt(1,reserva.getEmpleado_id());
-			st.setInt(2,reserva.getSala_id());
-			st.setDate(3,Date.valueOf(reserva.getFecha()));
-			st.setTime(4,Time.valueOf(reserva.getHora_inicio()));
-			st.setTime(5,Time.valueOf(reserva.getHora_final()));
+	public void actualizarReserva(Reserva reserva) throws SQLException {
+		String sql = "UPDATE RESERVA SET empleado_id=?,sala_id=?,fecha=?,hora_inicio=?,hora_final=? WHERE id=?";
+		try (Connection con = ConexionBD.getConnection(); PreparedStatement st = con.prepareStatement(sql)) {
+			st.setInt(1, reserva.getEmpleado_id());
+			st.setInt(2, reserva.getSala_id());
+			st.setDate(3, Date.valueOf(reserva.getFecha()));
+			st.setTime(4, Time.valueOf(reserva.getHora_inicio()));
+			st.setTime(5, Time.valueOf(reserva.getHora_final()));
 			st.setInt(6, reserva.getId());
+		} catch (SQLException e) {
+			System.out.println("Error al actualizar reserva");
+			e.printStackTrace();
+		}
+	}
+
+//DELETE
+	public void eliminarReserva(int res_id) throws SQLException {
+		String sql = "DELETE FROM RESERVA WHERE id=?";
+		try (Connection con = ConexionBD.getConnection(); PreparedStatement st = con.prepareStatement(sql)) {
+			st.setInt(1, res_id);
+			st.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("Error al eliminar reserva");
+			e.printStackTrace();
 		}
 	}
 

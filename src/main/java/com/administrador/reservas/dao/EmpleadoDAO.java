b5f6a24f57713733343283f2f7984955ed001d0e
ateres <aparejat17@gmail.com>
2025-06-26 11:41:23 +0200

    Creada C(Create) Empleado DAO

--- src/main/java/com/administrador/reservas/dao/EmpleadoDAO.java
+++ src/main/java/com/administrador/reservas/dao/EmpleadoDAO.java
@@ -1,5 +1,27 @@
 package com.administrador.reservas.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.administrador.reservas.ConexionBD;
+import com.administrador.reservas.modelo.Empleado;
+
 public class EmpleadoDAO {
+	// CREATE
+	public void agregarEmpleado(Empleado empleado) throws SQLException {
+		String sql = "INSERT INTO EMPLEADO(nombre,email,departamento) VALUES(?,?,?)";
+		try (Connection con = ConexionBD.getConnection(); PreparedStatement st = con.prepareStatement(sql)) {
+			st.setString(1, empleado.getNombre());
+			st.setString(1, empleado.getEmail());
+			st.setString(1, empleado.getDepartamento());
+			st.executeUpdate();
+
+		}catch (SQLException e) {
+			System.out.println("No se ha podido agregar empleado");
+			e.printStackTrace();
+		}
+
+	}
 
 }

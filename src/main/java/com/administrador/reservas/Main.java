377615ef35fad4c8d50bfb4b5482e2665b891e64
ateres <aparejat17@gmail.com>
2025-06-26 18:59:57 +0200

    Creacion de Menu
    Menu para elegir menu de sala, empleado y reserva

--- src/main/java/com/administrador/reservas/Main.java
+++ src/main/java/com/administrador/reservas/Main.java
@@ -10,12 +10,31 @@ import com.administrador.reservas.dao.SalaDAO;
 
 public class Main {
 
-	private static final Scanner scanner = new Scanner(System.in);
+	private static final Scanner scan = new Scanner(System.in);
 	private static final SalaDAO salaDAO = new SalaDAO();
 	private static final EmpleadoDAO empleadoDAO= new EmpleadoDAO();
 	private static final ReservaDAO reservaDAO=new ReservaDAO();
 
 	public static void main(String args[]) throws SQLException {
+		int opcion;
+		do {
+			System.out.println("\nMenu");
+			System.out.println("1.Menu Sala");
+			System.out.println("2.Menu Empleado");
+			System.out.println("3.Menu Reserva");
+			System.out.println("0. Salir");
+			opcion= scan.nextInt();
+			
+			switch (opcion) {
+			case 1 :;
+			case 2 :;
+			case 3 :;
+			case 0 :;
+			default:System.out.println("Opcion no Valida");
 
+			
+			}
+			
+		}while(opcion !=0);
 	}
 }
